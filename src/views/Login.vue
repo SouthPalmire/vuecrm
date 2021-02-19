981235213977201d3bc7516eb21dcb4d4688fed3
SouthPalmire <hexus.southpalmire@gmail.com>
2021-02-19 11:46:21 +0300

    fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,51 @@
 <template>
-  <form class="card auth-card" @submit="submitHandler">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate">
+        <input
+            id="email"
+            type="text"
+            v-model.trim="$v.email.$model"
+            :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+        >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small 
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >Поле Email не должно быть пустым</small>
+        <small 
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+        >Введите корретный Email</small>
       </div>
-
       <div class="input-field">
-        <input id="password" type="password" class="validate">
+        <input
+            id="password"
+            type="password"
+            v-model.trim="$v.password.$model"
+            :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
+        >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small 
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+        >
+          Введите пароль
+        </small>
+        <small 
+          class="helper-text invalid"
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+        >
+          Пароль должен быть {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}
+        </small>
       </div>
     </div>
-
     <div class="card-action">
       <div>
-        <button 
-          class="btn waves-effect waves-light auth-submit" 
-          type="submit"
+        <button
+            class="btn waves-effect waves-light auth-submit"
+            type="submit"
         >
           Войти
           <i class="material-icons right">send</i>
@@ -35,24 +61,32 @@
 </template>
 
 <script>
-  import {email, required, minLenght} from 'vuelidate/lib/validators'
+import {email, required, minLength} from 'vuelidate/lib/validators'
 
-  export default {
-    name: 'login',
-    data() {
-      return {
-        email: '',
-        password: ''
+export default {
+  name: 'login',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  validations: {
+    email: {email, required},
+    password: {required, minLength: minLength(6)}
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
       }
-    },
-    validations: {
-      email: {email, required},
-      password: {required, minLenght: minLenght(6)}
-    },
-    methods: {
-      submitHandler() {
-        this.$router.push('/')
+      const formData = {
+        email: this.email,
+        password: this.password
       }
+
+      console.log(formData)
+      this.$router.push('/')
     }
   }
+}
 </script>
