f03cbb21c66cea387435c290859c77239440a4a3
SouthPalmire <hexus.southpalmire@gmail.com>
2021-05-07 14:58:11 +0300

    fix

--- src/filters/date.filter.js
+++
@@ -1,6 +0,0 @@
-import { values } from "core-js/fn/dict";
-
-export default function dateFilter(value, format = 'date') {
-  const options = {}
-  return new Intl.DateTimeFormat('ru-Ru', options).format(new Date(value))
-}
--- src/utils/messages.js
+++
@@ -1,6 +0,0 @@
-export default {
-  'logout': 'END SESSION',
-  'auth/user-not-found': 'UNREGISTERED USER',
-  'auth/wrong-password': 'WRONG PASSWORD',
-  'auth/email-already-in-use': 'USER EXIST'
-}
