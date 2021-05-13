46b220f1044c250f5ae2464f8b91d18828d8b13f
SouthPalmire <hexus.southpalmire@gmail.com>
2021-05-13 12:17:07 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <router-view/>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
